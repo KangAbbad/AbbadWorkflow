800bc43d22c468892bb4414670de75af003526ff
KangAbbad <naufalabbad9@gmail.com>
2018-05-22 13:36:50 +0700

    :re up

---
+++ src/assets/js/fontawesome.js
@@ -0,0 +1,8 @@
+import fontawesome from '@fortawesome/fontawesome';
+import regular from '@fortawesome/fontawesome-free-regular';
+import solid from '@fortawesome/fontawesome-free-solid';
+import brands from '@fortawesome/fontawesome-free-brands';
+
+fontawesome.library.add(regular)
+fontawesome.library.add(solid)
+fontawesome.library.add(brands)
